a2f5a32924d8f116fc4476303e2917547dcc0df5
Tyler Duddy <tmd44@pitt.edu>
2020-04-01 14:41:14 -0400

    initialize bot and package info

---
+++ index.js
@@ -0,0 +1,10 @@
+const SlackBot = require('slackbots');
+const axios = require('axios');
+
+// init bot
+const bot = new SlackBot({
+    token: 'xoxb-604435993461-1042306273920-KfZ8rhgMINOPmJ6uAxmzmBxX',
+    name: 'toybot'
+})
+
+// Start Handler
