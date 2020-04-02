47518a91bd8335737609ae690d6f7117912f6288
Tyler Duddy <tmd44@pitt.edu>
2020-04-02 13:25:40 -0400

    add post listener

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const SlackBot = require('slackbots');
-const axios = require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// init webapp
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false}));
 
 // init bot
 const bot = new SlackBot({
@@ -12,7 +17,7 @@ bot.on('start', () => {
     const params = {
         icon_emoji: ":rolf:"
     }
-    bot.postMessageToChannel('toys', 'welcome 2 toys', params);
+    // bot.postMessageToChannel('toys', 'welcome 2 toys', params);
 });
 
 // err handler
@@ -26,15 +31,37 @@ bot.on('message', (data) => {
     handleMessage(data.text, data.user);
 });
 
+// listen for POSTs
+app.post('/toys', function(req, res) {
+    const body = req.boody.Body
+    res.set('Content-Type', 'text/plain')
+    res.send(`You sent: ${body} to Express`)
+});
+
+app.listen(3000, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('server started')
+});
+
 // respond to messages
 function handleMessage(message, user) {
-    if (user != 'UHTBJ1N7Q' || user != 'UHNBG7KJM' || user != 'UHTBM08CE') {
+    if (user != 'UHTBJ1N7Q' && user != 'UHNBG7KJM' && user != 'UHTBM08CE') {
         return;
     }
-    if (message.includes('new toy')) {
+    if (message.includes('congrats on new toy')) {
         let params = {
             icon_emoji: ':rolf:'
         }
         bot.postMessageToChannel('toys', ':rolf::rolf::rolf::rolf::rolf::rolf:\nwelcome to toys mfer\n:rolf::rolf::rolf::rolf::rolf::rolf:', params)
+    }
+
+    if (message.match(/.*should i (get|buy).*/gi)) {
+        let params = {
+            icon_emoji: ':rolf:'
+        }
+        bot.postMessageToChannel('toys', 'shut up and buy it mfer', params)
+        bot.postMessageToChannel('toys', ':rolf:', {icon_emoji: ':heart:'})
     }
 }
